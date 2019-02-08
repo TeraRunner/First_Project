0f26e915765c81fab280786aff3d9c07ee115fc7
Ruben Franco <rubenfrancosanchez@hotmail.es>
2019-02-08 20:01:28 +0000

    Updating tables

--- db/gym_activities.sql
+++ db/gym_activities.sql
@@ -2,25 +2,28 @@ DROP TABLE bookings;
 DROP TABLE activities;
 DROP TABLE members;
 
-CREATE TABLE members(
+CREATE TABLE members
+(
   id SERIAL8 PRIMARY KEY,
   first_name VARCHAR(255) NOT NULL,
   last_name VARCHAR(255) NOT NULL,
-  age NUMBER NOT NULL,
+  age INT8 NOT NULL,
   address TEXT NOT NULL,
   email VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE activities(
+CREATE TABLE activities
+(
   id SERIAL8 PRIMARY KEY,
-  title VARCHAR(255) NOT NULL,
+  activity VARCHAR(255) NOT NULL,
   instructor VARCHAR(255) NOT NULL,
-  day DATE(mm/dd),
-  start_time TIME(hh:mm) DEFAULT 16:00,
-  capacity DEFAULT 0,
+  day DATE NOT NULL,
+  start_time TIME NOT NULL,
+  capacity INT8 DEFAULT 0
 );
 
-CREATE TABLE bookings(
+CREATE TABLE bookings
+(
   id SERIAL8 PRIMARY KEY,
   member_id INT8 REFERENCES members(id),
   activity_id INT8 REFERENCES activities(id)
